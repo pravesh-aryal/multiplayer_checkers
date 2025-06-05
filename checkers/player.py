be53a8fa645bd3546b8976d83077b1462745bb82
Pravesh Aryal <pravesh@Mac.dhost.uta.edu>
2025-06-05 08:18:16 -0500

    removed all files new approach using fastapi now

--- checkers/player.py
+++
@@ -1,8 +0,0 @@
-
-
-class Player:
-    def __init__(self):
-        pass
-
-
-
--- static/js/socket.mjs
+++
@@ -1,4 +0,0 @@
-import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
-
-// const socket = io();
-export const socket = io();         

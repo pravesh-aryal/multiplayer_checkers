04ccea743e5ce49cff57d905bc0fcb254af76d93
Pravesh Aryal <pravesh@Mac.dhost.uta.edu>
2025-06-06 14:15:37 -0500

    query parameter and string validation

--- learning_fastapi/somepython/test.py
+++ learning_fastapi/somepython/test.py
@@ -1,2 +1,16 @@
-ls = [1,2,3,4,5,6]
-print(ls[0:9999])
+import sys
+
+a = [1, 2, 3, 4, 5,6]
+print("Initial reference count for object 'a':", sys.getrefcount(a))
+sys.getrefcount(a)
+b = a  # Now both 'a' and 'b' refer to the same list
+print("Reference count after assigning 'b = a':", sys.getrefcount(a))
+
+c = a  # Another reference
+print("Reference count after assigning 'c = a':", sys.getrefcount(a))
+
+del b  # Remove one reference
+print("Reference count after 'del b':", sys.getrefcount(a))
+
+del c  # Remove another reference
+print("Reference count after 'del c':", sys.getrefcount(a))

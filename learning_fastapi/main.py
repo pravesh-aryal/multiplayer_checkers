d5b841b42c597e76712073e6341e10384d3cff95
Pravesh Aryal <pravesh@Mac.dhost.uta.edu>
2025-06-18 10:42:11 -0500

    MVP goals updated

--- learning_fastapi/main.py
+++ learning_fastapi/main.py
@@ -1,20 +1,18 @@
-from typing import Annotated
-
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    *,
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str,
-    size: Annotated[float, Query(gt=0, lt=10.5)],
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if size:
-        results.update({"size": size})
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list = []
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
--- main.py
+++ main.py
@@ -1,58 +1,9 @@
-# from typing import Annotated
-# from fastapi import FastAPI, Query
-
-# app = FastAPI()
-
-# @app.get("/players/")
-# async def read_player(q: Annotated[list[str] | None, Query(
-#     title = "This is query str",
-#     description = "Query string for the item to saerch",
-#     min_length=3
-# )] = ['foo', 'bar']):
-#     results = {
-#         "players": [
-#             {
-#                 "player_id": "Bob",
-#             },
-#             {
-#                 "player_id": "Krishna"
-#             }
-#         ]
-#     }
-#     if q:
-#         results.update({
-#             "q" : q
-#         })
-#     return results
-
-
-import random
-from typing import Annotated
-
 from fastapi import FastAPI
-from pydantic import AfterValidator
+
 
 app = FastAPI()
 
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
 
-
-def check_valid_id(id: str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
-    return id
-
-
-@app.get("/items/")
-async def read_items(
-    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
-):
-    if id:
-        item = data.get(id)
-    else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "name": item}
+@app.get("/")
+def home():
+    return "This is homepage!"
